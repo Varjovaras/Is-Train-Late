6abde1de9a91fe9ea623e78ff74eec02c7ec54da
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-10-29 14:38:55 +0200

    show starting station of long distance trains

--- src/components/longDistanceTrains.tsx
+++ src/components/longDistanceTrains.tsx
@@ -1,31 +1,30 @@
 import type { Train } from "@/types/trainTypes";
 
 type TrainProps = {
-    longDistanceData: Train[];
+	longDistanceData: Train[];
 };
 
 export const LongDistanceTrains = ({
-    longDistanceData: passengerTrainData,
+	longDistanceData: passengerTrainData,
 }: TrainProps) => {
-    return (
-        <div className="p-8 mx-4">
-            <p className="pb-4 text-left">
-                Currently running long distance trains:{" "}
-            </p>
-            <div className="flex flex-wrap gap-4 ">
-                {passengerTrainData.length > 0 ? (
-                    passengerTrainData.map((train) => (
-                        <div key={`train-${train.trainNumber}`} className="">
-                            <div>
-                                {train.trainType.name}
-                                {train.trainNumber}
-                            </div>
-                        </div>
-                    ))
-                ) : (
-                    <div className="">no trains</div>
-                )}
-            </div>
-        </div>
-    );
+	return (
+		<div className="p-8 mx-4">
+			<p className="pb-4 text-left">Currently running long distance trains: </p>
+			<div className="grid sm:grid-cols-3  gap-4 ">
+				{passengerTrainData.length > 0 ? (
+					passengerTrainData.map((train) => (
+						<div key={`train-${train.trainNumber}`} className="">
+							<div>
+								{train.trainType.name}
+								{train.trainNumber} {train.departureDate.toString()}{" "}
+								{train.timeTableRows[0].station.name}
+							</div>
+						</div>
+					))
+				) : (
+					<div className="">no trains</div>
+				)}
+			</div>
+		</div>
+	);
 };
