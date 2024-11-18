87a85f6c7115dfef5c6c9bfc55bed1c602ab4ee7
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-11-18 20:11:15 +0200

    save the latest query

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,50 +1,56 @@
 import {
-    fetchAllPassengerTrainData,
-    // fetchAllPassengerTrainData,
-    fetchPassengerTrainData,
+	fetchAllPassengerTrainData,
+	// fetchAllPassengerTrainData,
+	fetchPassengerTrainData,
 } from "./queries/trainQueries";
 
 async function fetchTrainData() {
-    const trainData = await fetchAllPassengerTrainData();
-    const dataStr = JSON.stringify(trainData);
-    const path = "../../trainData.json";
-    await Bun.write(path, dataStr);
-    return trainData;
+	const trainData = await fetchAllPassengerTrainData();
+
+	return trainData;
 }
 
 async function main() {
-    console.log(`Starting server at ${new Date().toString()}`);
-    const trainData = await fetchTrainData();
+	console.log(`Starting server at ${new Date().toString()}`);
+	const trainData = await fetchTrainData();
 
-    const server = Bun.serve({
-        port: 8080,
-        static: {
-            "/api/trains": new Response(JSON.stringify(trainData)),
-        },
+	const server = Bun.serve({
+		port: 8080,
+		static: {
+			"/api/trains": new Response(JSON.stringify(trainData)),
+		},
 
-        fetch(_req) {
-            return new Response("404!");
-        },
-    });
+		fetch(_req) {
+			return new Response("404!");
+		},
+	});
 
-    console.log(`Server online o port ${server.port}`);
+	console.log(`Server online o port ${server.port}`);
 
-    // Update the time every minute.
-    setInterval(async () => {
-        console.log(`Updating server ${new Date().toString()}`);
-        const trainData = await fetchTrainData();
+	// Update the time every minute.
+	setInterval(async () => {
+		console.log(`Updating server ${new Date().toString()}`);
+		const file = await Bun.file("../../trainData.json").text();
 
-        server.reload({
-            static: {
-                "/api/trains": new Response(JSON.stringify(trainData)),
-            },
+		const trainData = await fetchTrainData();
 
-            fetch(_req) {
-                return new Response("404!");
-            },
-        });
-        console.log(`Server updated at ${new Date().toString()}`);
-    }, 60_000);
+		// console.log(file);
+
+		server.reload({
+			static: {
+				"/api/trains": new Response(JSON.stringify(trainData)),
+			},
+
+			fetch(_req) {
+				return new Response("404!");
+			},
+		});
+
+		const file2 = await Bun.file("../../trainData.json").text();
+		console.log(file === file2);
+
+		console.log(`Server updated at ${new Date().toString()}`);
+	}, 60_000);
 }
 
 main();
--- src/backend/src/queries/trainQueries.ts
+++ src/backend/src/queries/trainQueries.ts
@@ -1,160 +1,66 @@
 const GRAPHQL_ENDPOINT = "https://rata.digitraffic.fi/api/v2/graphql/graphql";
 import type { Train, TrainResponse } from "../../../types/trainQueryTypes";
 import {
-    unwantedTrainTypeNames,
-    type UnwantedTrainTypeName,
+	unwantedTrainTypeNames,
+	type UnwantedTrainTypeName,
 } from "../../../types/trainTypeNames";
 
 async function fetchData(query: string): Promise<Train[]> {
-    const cleanedQuery = query
-        .replace(/\s+/g, " ")
-        .replace(/\n/g, " ")
-        .replace(/\\/g, "")
-        .trim();
+	const cleanedQuery = query
+		.replace(/\s+/g, " ")
+		.replace(/\n/g, " ")
+		.replace(/\\/g, "")
+		.trim();
 
-    const data = await fetch(GRAPHQL_ENDPOINT, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept-Encoding": "gzip",
-        },
-        body: JSON.stringify({
-            query: cleanedQuery,
-        }),
-    });
+	const data = await fetch(GRAPHQL_ENDPOINT, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept-Encoding": "gzip",
+		},
+		body: JSON.stringify({
+			query: cleanedQuery,
+		}),
+	});
 
-    const trainResponse: TrainResponse = await data.json();
+	const trainResponse: TrainResponse = await data.json();
+	const dataStr = JSON.stringify(trainResponse);
+	const path = "../../trainData.json";
+	console.log("Writing to trainData.json");
+	await Bun.write(path, dataStr);
+	console.log("written :D");
 
-    const filteredTrains = filterUnwantedTraintypes(
-        trainResponse.data.currentlyRunningTrains,
-    );
+	const filteredTrains = filterUnwantedTraintypes(
+		trainResponse.data.currentlyRunningTrains,
+	);
 
-    return filteredTrains;
+	return filteredTrains;
 }
 
 function filterUnwantedTraintypes(trains: Train[]): Train[] {
-    const filteredTrains = trains.filter(
-        (train) =>
-            !unwantedTrainTypeNames.includes(
-                train.trainType.name as UnwantedTrainTypeName,
-            ),
-    );
-    console.log(
-        `filtered trainData example departuredate ${filteredTrains[0].departureDate}`,
-    );
+	const filteredTrains = trains.filter(
+		(train) =>
+			!unwantedTrainTypeNames.includes(
+				train.trainType.name as UnwantedTrainTypeName,
+			),
+	);
+	console.log(
+		`filtered trainData example departuredate ${filteredTrains[0].departureDate}`,
+	);
 
-    return trains;
+	return trains;
 }
 
 export async function fetchPassengerTrainData() {
-    const passengerQuery = await Bun.file(
-        "./src/queries/passengerQuery.gql",
-    ).text();
+	const passengerQuery = await Bun.file(
+		"./src/queries/passengerQuery.gql",
+	).text();
 
-    return await fetchData(passengerQuery);
+	return await fetchData(passengerQuery);
 }
 
 export async function fetchAllPassengerTrainData() {
-    const fullQuery = await Bun.file("./src/queries/fullQuery.gql").text();
+	const fullQuery = await Bun.file("./src/queries/fullQuery.gql").text();
 
-    return await fetchData(fullQuery);
+	return await fetchData(fullQuery);
 }
-
-// const fullQuery = `{
-//   currentlyRunningTrains(
-//     where: {and: [{operator: {shortCode: {equals: "vr"}}}, {or: [{trainType: {trainCategory: {name: {equals: "Commuter"}}}}, {trainType: {trainCategory: {name: {equals: "Long-distance"}}}}]}]}
-//   ) {
-//     cancelled
-//     commuterLineid
-//     departureDate
-//     runningCurrently
-//     timetableType
-//     trainNumber
-//     trainType {
-//       name
-//       trainCategory {
-//         name
-//       }
-//     }
-//     timeTableRows {
-//       type
-//       trainStopping
-//       commercialStop
-//       commercialTrack
-//       cancelled
-//       scheduledTime
-//       actualTime
-//       differenceInMinutes
-//       liveEstimateTime
-//       estimateSourceType
-//       unknownDelay
-//       station {
-//         passengerTraffic
-//         countryCode
-//         location
-//         name
-//         shortCode
-//         uicCode
-//         type
-//       }
-//       causes(where: {categoryCode: {name: {unequals: "HEL"}}}) {
-//         categoryCode {
-//           code
-//           name
-//           validFrom
-//           validTo
-//         }
-//         detailedCategoryCode {
-//           code
-//           name
-//           validFrom
-//           validTo
-//         }
-//         thirdCategoryCode {
-//           code
-//           name
-//           validFrom
-//           validTo
-//         }
-//       }
-//     }
-//     trainTrackingMessages {
-//       timestamp
-//       trackSectionCode
-//       nextTrackSectionCode
-//       previousTrackSectionCode
-//       type
-//       station {
-//         passengerTraffic
-//         countryCode
-//         location
-//         name
-//         shortCode
-//         uicCode
-//         type
-//       }
-//       nextStation {
-//         passengerTraffic
-//         countryCode
-//         location
-//         name
-//         shortCode
-//         uicCode
-//         type
-//       }
-//       previousStation {
-//         passengerTraffic
-//         countryCode
-//         location
-//         name
-//         shortCode
-//         uicCode
-//         type
-//       }
-//     }
-//     trainLocations(orderBy: {timestamp: DESCENDING}, take: 1) {
-//       speed
-//       location
-//     }
-//   }
-// }`;
