6adb82f86d0ca73a041ef9604b3e33fc27ff8a5c
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2025-01-17 11:13:41 +0200

    Add github link to footer

--- src/frontend/src/components/Footer.tsx
+++ src/frontend/src/components/Footer.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export const Footer = () => {
 	return (
-		<footer className="row-start-3 flex gap-6 p-8 flex-wrap items-center justify-center">
+		<footer className="w-full flex gap-4 sm:gap-0 flex-col sm:flex-row justify-between items-center mt-auto pt-4">
 			<a
-				className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-sm"
+				className="flex items-center gap-4 hover:underline hover:underline-offset-4 text-sm"
 				href="https://soundcloud.com/hardcore-bull"
 				target="_blank"
 				rel="noopener noreferrer"
@@ -12,11 +12,27 @@ export const Footer = () => {
 				<Image
 					aria-hidden
 					src="/hcbull_naama.png"
-					alt="Globe icon"
+					alt="Hardcore Bull Soundcloud icon"
 					width={40}
 					height={40}
 				/>
-				https://soundcloud.com/hardcore-bull {">"}
+				Soundcloud →
+			</a>
+
+			<a
+				className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-sm"
+				href="https://github.com/Varjovaras/Is-Train-Late"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<Image
+					aria-hidden
+					src="/github-mark.svg"
+					alt="Github icon"
+					width={40}
+					height={40}
+				/>
+				Github →
 			</a>
 		</footer>
 	);
