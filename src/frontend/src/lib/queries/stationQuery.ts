3bfb12158cceca3aafe5b913da171423d07047ba
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2025-01-29 14:58:20 +0200

    update station query

--- src/frontend/src/lib/queries/stationQuery.ts
+++ src/frontend/src/lib/queries/stationQuery.ts
@@ -1,20 +1,76 @@
 export const getStationQuery = (stationName: string) => {
-	return stationQuery
-		.replace("STATION_NAME", stationName.toString())
-		.replace(/\s+/g, " ")
-		.replace(/\n/g, " ")
-		.replace(/\\/g, "")
-		.trim();
+  return stationQuery
+    .replace("STATION_NAME", stationName.toString())
+    .replace(/\s+/g, " ")
+    .replace(/\n/g, " ")
+    .replace(/\\/g, "")
+    .trim();
 };
 
 const stationQuery = `{
-  stations(take: 1, where: {name: {equals: "Helsinki Asema"}}) {
-    name
+  stations(where: {name: {equals: "Helsinki Asema"}}) {
     passengerTraffic
     countryCode
     location
+    name
     shortCode
     uicCode
     type
+    stationMessages {
+      stationShortCode
+      station {
+        name
+      }
+      message {
+        id
+        version
+        creationDateTime
+        startValidity
+        endValidity
+        trainDepartureDate
+        trainNumber
+        train {
+          cancelled
+        }
+        audio {
+          text {
+            fi
+            sv
+            en
+          }
+          deliveryRules {
+            deliveryType
+            eventType
+            startDateTime
+            endDateTime
+            startTime
+            endTime
+            weekDays
+            deliveryAt
+            repetitions
+            repeatEvery
+          }
+          messageId
+          messageVersion
+        }
+        video {
+          text {
+            fi
+            sv
+            en
+          }
+          deliveryRules {
+            deliveryType
+            startDateTime
+            endDateTime
+            startTime
+            endTime
+            weekDays
+          }
+          messageId
+          messageVersion
+        }
+      }
+    }
   }
 }`;
