7e6822df90145d7ab80ef4e6f37b568709e89111
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2025-02-17 15:35:39 +0200

    center train timetables if no delay causes

--- src/frontend/src/components/train/LiveTrainPage.tsx
+++ src/frontend/src/components/train/LiveTrainPage.tsx
@@ -8,13 +8,24 @@ type LiveTrainPageProps = {
 };
 
 const LiveTrainPage = ({ train }: LiveTrainPageProps) => {
+  const timeTablesWithCauses = train.timeTableRows.filter(
+    (row) => row.causes !== null,
+  );
+  const hasDelayCauses = timeTablesWithCauses.length > 0;
+
   return (
     <div className="mx-auto flex flex-col">
       <TrainDetails train={train} />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <DelayInformation train={train} />
-        <Train train={train} forceShowAllStations />
-      </div>
+      {hasDelayCauses ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <DelayInformation train={train} />
+          <Train train={train} forceShowAllStations />
+        </div>
+      ) : (
+        <div className="max-w-2xl mx-auto w-full">
+          <Train train={train} forceShowAllStations />
+        </div>
+      )}
     </div>
   );
 };
