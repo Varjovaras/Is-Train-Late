c40025fa466c890ef02eaab4b394b7460de6901d
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2025-03-26 12:47:07 +0200

    Loading to screen readers

--- src/frontend/src/components/ui/Loading.tsx
+++ src/frontend/src/components/ui/Loading.tsx
@@ -1,8 +1,9 @@
 const Loading = () => {
   return (
     <div className="flex items-center justify-center min-h-[200px]">
-      {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
-      <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-foreground"></div>
+        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-foreground">
+        <span className="sr-only">Loading...</span>
+      </div>
     </div>
   );
 };
