2d5c7d22f82bff4b47a8d732563165e418e723c2
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2025-03-26 12:53:39 +0200

    small lint chang

--- src/frontend/src/components/ui/Loading.tsx
+++ src/frontend/src/components/ui/Loading.tsx
@@ -1,11 +1,11 @@
 const Loading = () => {
-  return (
-    <div className="flex items-center justify-center min-h-[200px]">
-        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-foreground">
-        <span className="sr-only">Loading...</span>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center min-h-[200px]">
+			<div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-foreground">
+				<span className="sr-only">Loading...</span>
+			</div>
+		</div>
+	);
 };
 
 export default Loading;
