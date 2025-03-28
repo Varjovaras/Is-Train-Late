5711ac7fd44aded6c8dbd45cfe39cfad87612ee2
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2025-03-28 10:38:08 +0200

    add suspense to map to not fail build

--- src/frontend/src/app/map/page.tsx
+++ src/frontend/src/app/map/page.tsx
@@ -1,22 +1,31 @@
 "use client";
 import dynamic from "next/dynamic";
 import Loading from "@/components/ui/Loading";
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 const TrainMap = dynamic(() => import("@/components/map/TrainMap"), {
-  ssr: false,
-  loading: () => <Loading />,
+	ssr: false,
+	loading: () => <Loading />,
 });
 
-const Page = () => {
-  const searchParams = useSearchParams();
-  const trainNumber = searchParams.get("train");
+const MapContent = () => {
+	const searchParams = useSearchParams();
+	const trainNumber = searchParams.get("train");
 
-  return (
-    <div className="w-full h-[calc(100vh-200px)] min-h-[500px] border border-foreground/20 rounded-lg overflow-hidden">
-      <TrainMap trainNumber={trainNumber || undefined} />
-    </div>
-  );
+	return (
+		<div className="w-full h-[calc(100vh-200px)] min-h-[500px] border border-foreground/20 rounded-lg overflow-hidden">
+			<TrainMap trainNumber={trainNumber || undefined} />
+		</div>
+	);
+};
+
+const Page = () => {
+	return (
+		<Suspense fallback={<Loading />}>
+			<MapContent />
+		</Suspense>
+	);
 };
 
 export default Page;
