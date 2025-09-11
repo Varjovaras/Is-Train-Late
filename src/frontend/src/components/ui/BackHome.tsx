c560a226b2213c061ffb1c24aae7e1b423c88cde
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2025-09-11 22:52:40 +0300

    smlal mobile change

--- src/frontend/src/components/ui/BackHome.tsx
+++ src/frontend/src/components/ui/BackHome.tsx
@@ -4,30 +4,32 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 
 const BackHome = () => {
-  const pathname = usePathname();
+    const pathname = usePathname();
 
-  const { translations, isLoading } = useTranslations();
+    const { translations, isLoading } = useTranslations();
 
-  const buttonText =
-    pathname !== "/" ? translations.backHome : translations.refreshTrainData;
-  const isHomePage = pathname === "/";
-  const buttonTextMobile = isHomePage
-    ? translations.refreshTrainDataMobile
-    : translations.backHomeMobile;
+    const buttonText =
+        pathname !== "/"
+            ? translations.backHome
+            : translations.refreshTrainData;
+    const isHomePage = pathname === "/";
+    const buttonTextMobile = isHomePage
+        ? translations.refreshTrainDataMobile
+        : translations.backHomeMobile;
 
-  return (
-    <Link
-      type="button"
-      href={"/"}
-      replace={isHomePage}
-      className={`px-2 sm:px-4 py-2 text-xs sm:text-sm border border-foreground rounded-md hover:bg-foreground hover:text-background transition-colors ${
-        isLoading ? "fade-out" : "fade-in"
-      }`}
-    >
-      <span className="hidden sm:inline">{buttonText}</span>
-      <span className="sm:hidden">{buttonTextMobile}</span>
-    </Link>
-  );
+    return (
+        <Link
+            type="button"
+            href={"/"}
+            replace={isHomePage}
+            className={`px-2 sm:px-4 py-2 text-xs sm:text-sm border border-foreground rounded-md hover:bg-foreground hover:text-background transition-colors ${
+                isLoading ? "fade-out" : "fade-in"
+            }`}
+        >
+            <span className="hidden sm:inline">{buttonText}</span>
+            <span className="sm:hidden text-[10px]">{buttonTextMobile}</span>
+        </Link>
+    );
 };
 
 export default BackHome;
