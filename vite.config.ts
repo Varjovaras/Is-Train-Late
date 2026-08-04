666f3e19df5ddd1a6dc23a0ffb49ed18b5d855a6
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2026-08-04 23:47:47 +0300

    update conflicting versions of react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
     server: {
         port: 5173,
     },
+    build: {
+        chunkSizeWarningLimit: 1100,
+    },
     resolve: {
         tsconfigPaths: true,
     },
